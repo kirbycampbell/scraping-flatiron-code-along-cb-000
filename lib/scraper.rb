020a9bac7daa09c19eddd7d5e07e55269c396e42
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-14 18:45:31 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,9 +13,9 @@ class Scraper
   def get_courses
     self.get_page.css(".post")
   end
-  
+
   def make_courses
-    doc.css(".post").each do |post|
+    self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
